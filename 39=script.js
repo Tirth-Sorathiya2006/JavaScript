97b6e97653ee0ec0433d7d1842cf67fdc184402d
Tirth-Sorathiya2006 <tirthsorathiya739@gmail.com>
2024-12-15 14:29:51 +0530

    Add files via upload

---
+++ 39=script.js
@@ -0,0 +1,5 @@
+// console.log() is not a part of the core JavaScript but it is a part of the V8 engine`s API which is injected at the run time of the JavaScript.
+
+//JavaScript is kind of a wraper on the C++. 
+
+//We can see the c++ code for the JavaScript in gitHub repo.
